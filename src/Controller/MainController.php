b1926c288adb8ea289d852afa4cf95acf2181ea4
Oriane-Toque <oriane.toque@gmail.com>
2021-11-27 21:17:52 +0100

    wip - section cv + method to download pdf done

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controller;
 
 use App\Repository\CollaborationRepository;
@@ -7,13 +8,16 @@ use App\Repository\ProjectRepository;
 use App\Repository\SkillRepository;
 use App\Repository\TechnologyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 /**
  * @Route(name="app_")
  */
-class MainController extends AbstractController {
+class MainController extends AbstractController
+{
 
     /**
      * OnePage with description & all projects
@@ -21,12 +25,13 @@ class MainController extends AbstractController {
      * @Route("/", name="home", methods={"GET"})
      * @return Response
      */
-    public function list(LangageRepository $lr, CollaborationRepository $cr, SkillRepository $sr, TechnologyRepository $tr, ProjectRepository $pr): Response {
+    public function list(LangageRepository $lr, CollaborationRepository $cr, SkillRepository $sr, TechnologyRepository $tr, ProjectRepository $pr): Response
+    {
 
         // récupération des projets pour la liste
         $projects = $pr->findProjects();
 
-        for($i=0; $i < count($projects); $i++) {
+        for ($i = 0; $i < count($projects); $i++) {
             // transformation de la chaine langages en tableau
             $projects[$i]['languages'] = explode(',', $projects[$i]['languages']);
             // transformation de la chaine technologies en tableau
@@ -40,5 +45,19 @@ class MainController extends AbstractController {
             "skills" => $sr->findAll(),
             "projects" => $projects,
         ]);
+    }
+
+    /**
+     * @Route("/cv", name="cv", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function download(): Response
+    {
+        // load the file from the filesystem
+        $file = new File($this->getParameter('kernel.project_dir').'/public/files/cv_oriane_toque.pdf');
+
+        // rename the downloaded file
+        return $this->file($file, 'curriculum_vitae.pdf', ResponseHeaderBag::DISPOSITION_ATTACHMENT);
     }
 }
