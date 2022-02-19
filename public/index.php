1bba7b75568fe0dfdc8448d2737497424a790da7
Oriane-Toque <oriane.toque@gmail.com>
2022-02-19 18:52:44 +0100

    ssl certificate

--- public/index.php
+++ public/index.php
@@ -1,8 +1,18 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+Request::setTrustedProxies(
+    // trust *all* requests (the 'REMOTE_ADDR' string is replaced at
+    // run time by $_SERVER['REMOTE_ADDR'])
+    ['127.0.0.1', 'REMOTE_ADDR'],
+
+    // if you're using ELB, otherwise use a constant from above
+    Request::HEADER_X_FORWARDED_AWS_ELB
+);
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
